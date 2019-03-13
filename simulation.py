4082ec521c086c4fe650d8824baf131868722ca8
Lester Fan <hiiamlife@gmail.com>
2019-03-13 14:36:40 -0400

    Anton's changes

--- simulation.py
+++ simulation.py
@@ -1,8 +1,8 @@
 import networkx as nx 
 import random
 
-def simulate_once(k, m, f, s):
-    assert(k <= m <= f <= s)
+def simulate_once(k, n, f, s):
+    assert(k <= n <= f <= s)
     # Make an undirected, connected graph of size s
     G = nx.Graph()
     for i in range(s):
@@ -10,14 +10,20 @@ def simulate_once(k, m, f, s):
     for i in range(s):
         for j in range(i + 1, s):
             G.add_edge(i, j)
+    time_elapsed = 0
     # Disseminate
-    F = random.sample(range(s), f)
-    for i in random.sample(F, m):
-        G.nodes[i]['has_bit'] = True
+    F = set(random.sample(range(s), f))
+    num_distributed = 0
+    curr_index = random.randint(0, s - 1)
+    while num_distributed < n:
+        if curr_index in F and G.nodes[curr_index]['has_bit'] == False:
+            num_distributed += 1
+            G.nodes[curr_index]['has_bit'] = True
+        curr_index = random.choice(list(G.adj[curr_index]))
+        time_elapsed += 1
     # Collect
     num_collected = 0
     curr_index = random.randint(0, s - 1)
-    time_elapsed = 0
     while num_collected < k:
         if G.nodes[curr_index]['has_bit'] == True:
             num_collected += 1
@@ -27,11 +33,15 @@ def simulate_once(k, m, f, s):
     return time_elapsed
 
 if __name__ == "__main__":
-    num_times = 30
-    k = 5
-    m = 10
+    ks = [5, 10, 10, 10, 9, 11]
+    ns = [10, 14, 13, 15, 14, 14]
+    m = 1000
     f = 20
     s = 30
-    for i in range(num_times):
-        print(simulate_once(k, m, f, s))
+    for i in range(len(ks)):
+        k, n = ks[i], ns[i]
+        num_times = int(m/k)
+        total_time = sum([simulate_once(k, n, f, s) for _ in range(num_times)])
+        print(f'For k = {k}, n = {n}, f = {f}, s = {s}, m = {m}, total_time = {total_time}')
     
+
